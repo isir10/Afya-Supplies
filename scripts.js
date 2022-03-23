78f5a3878132b8016a8fa156a5e46351e55c631e
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-03-23 20:45:33 +0300

    Add changes

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#btn1").click(function(event){
+        event.preventDefault();
+
+        var value1 = parseInt($("#value1").val());
+        
+    })
+    $("#btn2").click(function(event){
+        event.preventDefault();
+        var value2 = parseInt($("#value2").val());
+        
+
+    })
+    
+
+})
