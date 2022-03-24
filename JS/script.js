dba67693a690883b56079764065b7f59732a1617
isir10 <isir.malak@moringaschool.com>
2022-03-24 16:07:18 +0300

    modified html css and js

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ var mainImg =document.getElementById('main-image');
 var smallImg =document.getElementsByClassName('small-img');
 
 smallImg[0].onclick= function(){
-    mainImg.src = smallImg[0].src;
+ mainImg.src = smallImg[0].src;
 }
 smallImg[1].onclick= function(){
     mainImg.src = smallImg[1].src;
@@ -10,6 +10,23 @@ smallImg[1].onclick= function(){
 smallImg[2].onclick= function(){
     mainImg.src = smallImg[2].src;
 }
-smallImg[3].onclick= function(){
-    mainImg.src = smallImg[3].src;
+smallImg[2].onclick= function(){
+    mainImg.src = smallImg[2].src;
 }
+
+ let mainImage =document.getElementById('main');
+
+ let smallImage =document.getElementsByClassName('small-Img');
+
+ smallImage[0].onclick= function(){
+   mainImage.src = smallImage[0].src;
+ }
+ smallImage[1].onclick= function(){
+    mainImage.src = smallImage[1].src;
+ }
+  smallImage[2].onclick= function(){
+    mainImage.src = smallImage[2].src;
+  }
+ smallImage[3].onclick= function(){ 
+    mainImage.src = smallImage[3].src;
+ }
